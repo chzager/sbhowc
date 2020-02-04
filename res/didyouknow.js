915144035535388c65c49a089b6ce94a10c58149
Suppenhuhn79 <ch.zager@web.de>
2020-02-04 21:28:33 +0100

    typo

--- res/didyouknow.js
+++ res/didyouknow.js
@@ -4,7 +4,7 @@ let didyouknowHints = [
 	"The recommended warband size is 300 points with no more then 1/3 personalitites.",
 	"Song of Blades an Heroes Online Warband Creator checks if the number of your warbands personality points is regular.",
 	"There is an option to switch off highlighting of personalities in the options-menu.",
-	"To delete a unit, click on it\'s points and select \"Delete unit\" from the menu.",
+	"To delete a unit, click on it\'s points and select \"Remove unit\" from the menu.",
 	"You can sort the warband\'s units by clicking on a unit\'s points and select \"Move unit up\" or \"Move unit down\" from the menu.",
 	"A special rule underlined in red is not included in the selection of rulebooks you have chosen in the options-menu."
 ];
