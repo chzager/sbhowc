b39942a107d9c5a233b3be48d8577595bd74eca0
Suppenhuhn79 <ch.zager@web.de>
2020-12-17 23:50:18 +0100

    restructuring

--- topmenu.js
+++ topmenu.js
@@ -2,54 +2,21 @@
 
 var topMenu = {};
 
-topMenu.undoClick = function(clickEvent) /* OK */
-{
-	if (owc.undoer.canUndo === true)
-	{
-		owc.warband.fromString(owc.undoer.undo(), owc.resources);
-		printWarband();
-	}
-};
-
-topMenu.newWarbandClick = function(clickEvent) /* OK */
+topMenu.newWarbandClick = function (clickEvent)
 {
 	let params = {};
 	params[urlParam.pid] = generateNewPid();
 	window.open(window.location.setParams(params, false, false));
 };
 
-topMenu.printPreviewClick = function(clickEvent) /* todo */
+topMenu.printPreviewClick = function (clickEvent)
 {
 	let params = {};
 	params[urlParam.print] = "1";
 	window.open(window.location.setParams(params, true, false));
 };
 
-topMenu.warbandToFileClick = function(clickEvent) /* OK */
-{
-	fileIo.offerFileToClient(owc.warband.name.notEmpty(owc.resources.defaultText("defaultWarbandName")) + ".sbh.txt", owc.warband.toString());
-};
-
-topMenu.warbandFromFileClick = function(clickEvent) /* OK */
-{
-	fileIo.requestClientFile(clickEvent, (fileEvent) => {
-	let warbandCode = fileEvent.target.result;
-	try
-	{
-		owc.warband.fromString(warbandCode, owc.resources);
-		owc.undoer.clear();
-		printWarband();
-	}
-	catch (ex)
-	{
-		console.error(ex);
-		window.alert("Your file does not provide a valid warband code.");
-	};
-	}
-	);
-};
-
-topMenu.showWarbandCodeClick = function(clickEvent)
+topMenu.showWarbandCodeClick = function (clickEvent)
 {
 	function _showWarbandCode()
 	{
@@ -65,7 +32,7 @@ topMenu.showWarbandCodeClick = function(clickEvent)
 	};
 };
 
-topMenu.showSettingsClick = function(clickEvent)
+topMenu.showSettingsClick = function (clickEvent)
 {
 	function _showSettings()
 	{
@@ -79,4 +46,38 @@ topMenu.showSettingsClick = function(clickEvent)
 	{
 		_showSettings();
 	};
+};
+
+topMenu.undoClick = function (clickEvent)
+{
+	if (owc.undoer.canUndo === true)
+	{
+		owc.warband.fromString(owc.undoer.undo(), owc.resources);
+		printWarband();
+	}
+};
+
+topMenu.warbandFromFileClick = function (clickEvent)
+{
+	fileIo.requestClientFile(clickEvent, (fileEvent) =>
+	{
+		let warbandCode = fileEvent.target.result;
+		try
+		{
+			owc.warband.fromString(warbandCode, owc.resources);
+			owc.undoer.clear();
+			printWarband();
+		}
+		catch (ex)
+		{
+			console.error(ex);
+			window.alert("Your file does not provide a valid warband code.");
+		};
+	}
+	);
+};
+
+topMenu.warbandToFileClick = function (clickEvent)
+{
+	fileIo.offerFileToClient(owc.warband.name.notEmpty(owc.resources.defaultText("defaultWarbandName")) + ".sbh.txt", owc.warband.toString());
 };
