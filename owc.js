45782f3726f0c2facfbdea302f10d6dcfa9795d1
Suppenhuhn79 <ch.zager@web.de>
2021-02-27 20:39:03 +0100

    feb21 release

--- owc.js
+++ owc.js
@@ -18,7 +18,7 @@ let owc =
 	"meta":
 	{
 		"TITLE": "Online Warband Creator for Song of Blades and Heroes",
-		"VERSION": "Feb21 draft",
+		"VERSION": "Feb21 release",
 		"ORIGIN": "https://suppenhuhn79.github.io/sbhowc"
 	},
 	"warband": null
