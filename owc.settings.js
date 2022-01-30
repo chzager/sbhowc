88f51ce20e51e5eaa0ba9f6d24f59960a5c792f7
Suppenhuhn79 <ch.zager@web.de>
2022-01-30 20:56:12 +0100

    refactored: settings

--- owc.settings.js
+++ owc.settings.js
@@ -5,80 +5,56 @@ Licensed unter the GNU Affero General Public License, Version 3
 See the full license text at https://www.gnu.org/licenses/agpl-3.0.en.html
  */
 
-owc.settings =
-{
-	STORAGE_KEY: "owc_settings"
-};
-
-owc.settings.init = function ()
-{
-	owc.settings.ruleScope = ["sbh", "sgd", "sww"];
-	owc.settings.options =
-	{
-		highlightPersonalities: true,
-		personalitiesInPoints: false,
-		applyRuleChecks: true,
-		warbandcodeIncludesComments: true
-	};
-	owc.settings.language = "en";
-	owc.settings.defaults = {
-		quality: 3,
-		combat: 3
-	};
-	owc.settings.viewMode = (owc.ui.isTouchDevice) ? "classictouch" : "classic";
-};
-
-owc.settings.toJson = function ()
-{
-	let result = {};
-	for (let key in owc.settings)
-	{
-		if ((key !== "STORAGE_KEY") && (typeof owc.settings[key] !== "function"))
-		{
-			result[key] = owc.settings[key];
+owc.settings = {
+	STORAGE_KEY: "owc_settings",
+	init: () => {
+		const DEFAULT_SETTINGS = {
+			ruleScope: ["sbh", "sgd", "sww"],
+			options: {
+				highlightPersonalities: true,
+				personalitiesInPoints: false,
+				applyRuleChecks: true,
+				warbandcodeIncludesComments: true
+			},
+			language: "en",
+			defaults: {
+				quality: 3,
+				combat: 3
+			},
+			viewMode: "classic"
 		};
-	};
-	return result;
-};
-
-owc.settings.fromJson = function (jsonObject)
-{
-	function copyJson(source, target)
-	{
-		for (let key in source)
+		owc.settings.fromJson(DEFAULT_SETTINGS);
+		owc.settings.viewMode = (owc.ui.isTouchDevice) ? "classictouch" : "classic";
+	},
+	toJson: () => {
+		let result = {};
+		for (let key in owc.settings)
 		{
-			switch (source[key].constructor.name)
+			if ((key !== "STORAGE_KEY") && (typeof owc.settings[key] !== "function"))
 			{
-				case "Function":
-					break;
-				case "Object":
-					copyJson(source[key], target[key]);
-					break;
-				default:
-					target[key] = source[key];
-			};
-		};
-	};
-	copyJson(jsonObject, owc.settings);
-};
-
-owc.settings.save = function ()
-{
-	if (!!localStorage)
-	{
-		localStorage.setItem(owc.settings.STORAGE_KEY, JSON.stringify(owc.settings.toJson()));
-	};
-};
-
-owc.settings.load = function ()
-{
-	owc.settings.init();
-	if (!!localStorage)
-	{
-		let storedSettings = JSON.parse(localStorage.getItem(owc.settings.STORAGE_KEY));
-		if (!!storedSettings)
+				result[key] = owc.settings[key];
+			}
+		}
+		return result;
+	},
+	fromJson: (jsonObject) => {
+		Object.assign(owc.settings, jsonObject);
+	},
+	save: () => {
+		if (!!localStorage)
 		{
-			owc.settings.fromJson(storedSettings);
-		};
-	};
+			localStorage.setItem(owc.settings.STORAGE_KEY, JSON.stringify(owc.settings.toJson()));
+		}
+	},
+	load: () => {
+		owc.settings.init();
+		if (!!localStorage)
+		{
+			let storedSettings = JSON.parse(localStorage.getItem(owc.settings.STORAGE_KEY));
+			if (!!storedSettings)
+			{
+				owc.settings.fromJson(storedSettings);
+			}
+		}
+	}
 };
