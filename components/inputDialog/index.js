f6aa086bdf2107e0545bc40763a32ab3efd98266
chzager <ch.zager@web.de>
2025-12-16 19:43:23 +0100

    Refactoring and documentation done

--- components/inputDialog/index.js
+++ components/inputDialog/index.js
@@ -1,43 +1,39 @@
 // @ts-check
-// DOC entire file
+/**
+ * A modal dialog for user inputs (text or numbers).
+ */
 const inputDialog = new class
 {
+	/**
+	 * An HTML element that locks the entire document (through overlaying the full viewport) and
+	 * holds the actual input dialog element.
+	 */
 	#veil = document.body.appendChild(makeElement(
 		"div#inputDialogVeil",
 		{
-			onclick: (/** @type {PointerEvent} */evt) =>
-			{
-				if (evt.target === this.#veil)
-				{
-					this.close();
-				}
-			}
+			style: "display:none",
+			onclick: (/** @type {PointerEvent} */evt) => (evt.target === this.#veil) && this.close()
 		}
 	));
 
-	/** @type {HTMLElement} */
+	/** The HTML element that renders the input dialog on the document. @type {HTMLElement} */
 	#element;
 
 	/**
-	 */
-	constructor()
-	{
-		this.close();
-	}
-
-	/**
+	 * Pops up an input dialog.
+	 *
 	 * @overload
-	 * @param {"text"} type
-	 * @param {string} title
-	 * @param {string} [value]
+	 * @param {"text"} type Type of the input value.
+	 * @param {string} title Title of the dialog.
+	 * @param {string} [value] Preset value.
 	 * @returns {Promise<string>}
 	 *
 	 * @overload
-	 * @param {"number"} type
-	 * @param {string} title
-	 * @param {number} [value]
-	 * @param {number} [min]
-	 * @param {number} [max]
+	 * @param {"number"} type Type of the input value.
+	 * @param {string} title Title of the dialog.
+	 * @param {number} [value] Preset value.
+	 * @param {number} [min] Minimum allowed number.
+	 * @param {number} [max] Maximum allowed number.
 	 * @returns {Promise<number>}
 	 */
 	prompt (type, title, value, ...args)
@@ -65,20 +61,15 @@ const inputDialog = new class
 				},
 				onOk: () => confirm(),
 			};
-			switch (type)
+			if (type === "number")
 			{
-				case "number":
-					Object.assign({
-						min: args[0],
-						max: args[1],
-					}, snippetData);
-					break;
+				Object.assign(snippetData, { min: args[0], max: args[1], });
 			}
 			this.#element = /** @type {HTMLElement} */(pageSnippets.produce("/components/inputPrompt", snippetData));
-			/* currently there is no definitive way to react when then virtual keyboard on touch device shrinks available height,
-			so we set the position of the prompt menu to the upper quarter */
 			this.#veil.appendChild(this.#element);
 			this.#veil.style.display = "block";
+			// Currently there is no definitive way to react when then virtual keyboard on a touch device
+			// shrinks available height, so we set the position of the prompt menu to the upper quarter.
 			this.#element.style.top = Math.round((window.innerHeight / 4) - (this.#element.offsetHeight / 2)) + "px";
 			this.#element.style.left = Math.round((visualViewport.width - this.#element.offsetWidth) / 2) + "px";
 			const inputElement = this.#element.querySelector("input");
@@ -94,10 +85,12 @@ const inputDialog = new class
 		});
 	}
 
+	/**
+	 * Closes the current input dialog (removes the HTML element from the document and hides the veil).
+	 */
 	close ()
 	{
 		this.#veil.style.display = "none";
 		this.#element?.remove();
 	}
-
 };
